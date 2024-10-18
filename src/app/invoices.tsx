01773dadf70cc8e69b49cae854553a21fbf8b832
alexandre-dev28 <axel.business29@gmail.com>
2024-10-18 14:58:05 +0200

    refactor code

--- src/app/invoices.tsx
+++ src/app/invoices.tsx
@@ -1,4 +1,4 @@
-import { File, ListFilter, PlusCircle } from 'lucide-react';
+import { PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 import {
@@ -8,57 +8,19 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 import { Link } from '@tanstack/react-router';
 import { InvoiceDataTable } from '@/components/invoice-datatable/data-table';
-import { Skeleton } from '@/components/ui/skeleton';
 
 export function HomePage() {
   return (
     <div>
-      <div className="flex items-center">
-        <div className="ml-auto flex items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="sm" className="h-7 gap-1">
-                <ListFilter className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Filter
-                </span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem checked>
-                Active
-              </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <Button size="sm" variant="outline" className="h-7 gap-1">
-            <File className="h-3.5 w-3.5" />
-            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Export
-            </span>
-          </Button>
-        </div>
-      </div>
       <Card x-chunk="dashboard-06-chunk-0" className="mt-4">
         <CardHeader className="flex justify-between flex-row">
           <div>
-            <CardTitle>Products</CardTitle>
+            <CardTitle className={'font-bold text-2xl'}>Invoices</CardTitle>
             <CardDescription>
-              Manage your products and view their sales performance.
+              Manage your invoices and view their sales performance.
             </CardDescription>
           </div>
           <Link to="/invoices/create-invoice">
